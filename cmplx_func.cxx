8414856fa36a4d07206fd4f9faf2f1924aca1a8c
AbdulHamada <ABDUL.HAMADA@hhu.de>
2015-11-04 18:50:28 +0100

    Update cmplx_func.cxx

--- cmplx_func.cxx
+++ cmplx_func.cxx
@@ -1,13 +1,39 @@
+
 #include <iostream>
 #include <complex>
+//#define pi 3.14
+//#define n 100
+
+using namespace std;
+
+complex <double> tanfunc(double Winkel)
+{
+  complex <double> c= complex <double> (1.0,1.0);
+ return tan(Winkel*c); 
+}
 
 int main(){
-
+   const double pi=3.14;
+   const int n=100;
+   complex <double> arr_z[n];
+   //double phi=0, delta_phi=2*pi/n;
+   double phi=-pi, delta_phi=2*pi/n;
+   complex <double> ii =complex <double> (0,1);
   // Example how to declare a single complex variable:
-  complex<double> c;
-
+  
+   
   // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
+  for (int i=0; i<n; i++)
+  {
+   arr_z[i]=tanfunc(phi); 
+   // arr_z[i]=exp(ii*phi);
+    phi=phi+delta_phi;
+  }
+  for(int i=0; i<n; i++)
+  {
+    cout<<arr_z[i];
+    cout<<"\t "<< real(arr_z[i])<<"\t"<<imag(arr_z[i])<<"\t"<<abs(arr_z[i])<<endl;
+  }
 
   return 0;
 }
